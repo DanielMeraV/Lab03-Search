fd57575a18c1216a3afbc51275b7aa0d0d34b261
Daniel Mera <daniel.mera@epn.edu.ec>
2022-12-27 15:41:10 -0500

    first commit

---
+++ Final Exercise.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Lab03_Search;
+class FinalExercise
+{
+    static void Main(string[] args)
+    {
+        // Arreglo desordenado
+        Console.WriteLine("Algoritmo ejercicio final: ");
+        int[] A = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < A.Length; i++)
+        {
+            A[i] = random.Next(1, 201);
+        }
+
+        // Impresion del arreglo
+        Console.WriteLine("Arreglo desordenado: ");
+        for (int i = 0; i < A.Length; i++)
+        {
+            Console.Write($"A[{i}]={A[i]}, ");
+        }
+
+        var horaInicio = DateTime.Now; // Hora de inicio
+        Stopwatch time = new Stopwatch(); // Reloj para el tiempo de ejecucion
+        time.Start(); // Inicio de reloj
+
+        // Busqueda de los elementos
+        int numBuscado, busquedasExitosas = 0, busquedasFallidas = 0, posicionEncontrada;
+        String StringNumBuscados = "\nNumeros a buscar: ";
+        for(int i = 0; i < 50; i++)
+        {
+            numBuscado = random.Next(1, 201); // Elemento a buscar random
+            StringNumBuscados += numBuscado + "  ";
+            posicionEncontrada = BusquedaLinealID(A, A.Length, numBuscado);
+            if (posicionEncontrada == -1)
+            {
+                busquedasFallidas++;
+            }
+            else
+            {
+                busquedasExitosas++;
+            }
+        }
+
+        // Porcentajes de exito
+        double porcentajeExito = (busquedasExitosas*100)/50;
+        double porcentajeFallido = (busquedasFallidas*100)/50;
+
+        // Ordenar Array
+        Array.Sort(A);
+
+        // Impresion de Resultados
+        Console.WriteLine("\n\n" + StringNumBuscados); // Numeros aleatorios
+        Console.WriteLine("Busquedas existosas: " + busquedasExitosas);
+        Console.WriteLine("Busquedas fallidas: " + busquedasFallidas);
+        Console.WriteLine("Busquedas Totales: " + (busquedasExitosas + busquedasFallidas));
+        Console.WriteLine("Porcentaje de exito en las busquedas: " + porcentajeExito + "%");
+        Console.WriteLine("Porcentaje de fallas en las busquedas: " + porcentajeFallido + "%");
+
+        Console.WriteLine("Arreglo ordenado: "); // Arreglo ordenado
+        for (int i = 0; i < A.Length; i++)
+        {
+            Console.Write($"A[{i}]={A[i]}, ");
+        }
+
+        time.Stop(); // Fin del reloj
+        Console.WriteLine("\n\nTiempo total de ejecucion: " + time.Elapsed.TotalMilliseconds + " ms");
+        var horaFin = DateTime.Now; // Hora de finalizacion
+
+        // Impresion de la hora de inicio y fin
+        Console.WriteLine($"Hora de inicio: {horaInicio}");
+        Console.WriteLine($"Hora de fin: {horaFin}");
+    }
+
+    static int BusquedaLinealID(int[] A, int n, int clave)
+    {
+        int i;
+        for (i = 0; i < n; i++)
+            if (A[i] == clave)
+                return i;
+        return -1;
+    }
+
+}
---
+++ Lineal Search.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Lab03_Search;
+class LinealSearch
+{
+    static void Main1(string[] args)
+    {
+        // Arreglo desordenado
+        Console.WriteLine("Algoritmo de busqueda lineal: ");
+        int[] A = { 79, 21, 15, 99, 88, 65, 75, 85, 76, 46, 84, 24, 25, 57, 13, 10, 78, 10, 65, 69 };
+
+        // Impresion del arreglo
+        Console.WriteLine("Arreglo desordenado: ");
+        for (int i = 0; i < A.Length; i++)
+        {
+            Console.Write($"A[{i}]={A[i]}, ");
+        }
+
+        // Leer elemento a buscar por teclado
+        Console.Write("\nIngrese el elemento a buscar: ");
+        int elemBuscado = int.Parse(Console.ReadLine());
+
+        var horaInicio = DateTime.Now; // Hora de inicio
+        Stopwatch time = new Stopwatch(); // Reloj para el tiempo de ejecucion
+        time.Start(); // Inicio de reloj 
+
+        // Busqueda y impresion del elemento encontrado
+        int posicionEncontrada = BusquedaLinealID(A, A.Length, elemBuscado);
+        if (posicionEncontrada == -1)
+        {
+            Console.WriteLine("\nElemento no encontrado");
+        }
+        else
+        {
+            Console.WriteLine($"\nElemento encontrado en la posicion: A[{posicionEncontrada}]={A[posicionEncontrada]}");
+        }
+
+        time.Stop(); // Fin del reloj
+        Console.WriteLine("Tiempo total de ejecucion: " + time.Elapsed.TotalMilliseconds + " ms");
+        var horaFin = DateTime.Now; // Hora de finalizacion
+
+        // Impresion de la hora de inicio y fin
+        Console.WriteLine($"Hora de inicio: {horaInicio}");
+        Console.WriteLine($"Hora de fin: {horaFin}");
+    }
+
+    static int BusquedaLinealID(int[] A, int n, int clave)
+    {
+        int i;
+        for (i = 0; i < n; i++)
+            if (A[i] == clave)
+                return i;
+        return -1;
+    }
+
+}
